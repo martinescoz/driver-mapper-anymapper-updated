6371a61e93d47b1a26ed86b99ddf8ef3a5f2aa7f
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 15:34:56 +0000

    Implement responsive design issues on mobile devices

--- anymapper/pe.cpp
+++ anymapper/pe.cpp
@@ -164,4 +164,4 @@ bool pe::pe::resolve_imports(
 		{
 			auto thunk_data =
 				reinterpret_cast< PIMAGE_IMPORT_BY_NAME >(
-					( uint64_t )pdos_header + first_thunk_original->u1.AddressOfData );
+					( uint64_t )pdos_header + first_thunk_original->u1.AddressOfData
