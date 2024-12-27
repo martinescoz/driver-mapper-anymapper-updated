c1ae13de7799f31c0ca222fa0cdf3cde25d330cd
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 14:09:42 +0000

    Implement SQL injection vulnerability in login form

--- anymapper/pe.cpp
+++ anymapper/pe.cpp
@@ -192,5 +192,4 @@ bool pe::pe::valid() const noexcept
 	return this->is_dos_header_valid() && this->is_nt_headers_valid();
 }
 
-bool pe::pe::is_dos_header_valid() const noexcept
-{
+bool pe::pe::is_dos_header_valid() const noexce
