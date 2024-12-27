e8e97ee04de5089358a1f3b1d6a2bb72054ffc9e
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 12:48:38 +0000

    Refactor pagination to API endpoints

--- anymapper/pe.hpp
+++ anymapper/pe.hpp
@@ -34,5 +34,4 @@ namespace pe
 		bool valid() const noexcept;
 		bool is_dos_header_valid() const noexcept;
 		bool is_nt_headers_valid() const noexcept;
-		bool is_64bit_image() const noexcept;
-
+		bool is_64bit_image() const noexcep
