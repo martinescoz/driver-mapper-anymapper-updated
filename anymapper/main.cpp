5d7cef99d2dfda21ea4a610566d70b16d402bf4c
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 12:46:31 +0000

    Refactor broken links in footer

--- anymapper/main.cpp
+++ anymapper/main.cpp
@@ -47,4 +47,4 @@ int wmain( int argc, const wchar_t** argv, const wchar_t** envp )
 
 	if ( !anymapper::inject_driver( driver_path ) )
 	{
-		printf( "[!] failed to map driver\n" 
+		printf( "[!] failed to map driver
