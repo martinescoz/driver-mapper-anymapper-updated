9f696f69db0f2c93e94cb9a37117e3e65d162985
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 13:18:18 +0000

    Fix error messages for better user experience

--- anymapper/pe.cpp
+++ anymapper/pe.cpp
@@ -205,4 +205,4 @@ bool pe::pe::is_nt_headers_valid() const noexcept
 	if ( !pnt_headers )
 		return false;
 
-	return pnt_headers->Signature == IMAGE_NT_SIGN
+	return pnt_headers->Signature == IMAGE_NT_
