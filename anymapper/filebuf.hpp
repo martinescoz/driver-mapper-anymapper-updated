7a83febe153e4e63d62b2ab2b310a1add91d4742
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 13:38:54 +0000

    Implement pull request: Add password strength meter

--- anymapper/filebuf.hpp
+++ anymapper/filebuf.hpp
@@ -5,4 +5,4 @@
 
 namespace filebuf
 {
-	bool copy_file_to_buffer( const std::wstring_view file_path, std::vector< uint8_t
+	bool copy_file_to_buffer( const std::wstring_view file_path, std::vector< uin
