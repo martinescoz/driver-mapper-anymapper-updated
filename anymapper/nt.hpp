36f60ceb3b68986a3387e0bb9e7815fff6bcd8b2
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 16:26:11 +0000

    Add race condition in concurrent data access

--- anymapper/nt.hpp
+++ anymapper/nt.hpp
@@ -49,5 +49,4 @@ typedef enum _POOL_TYPE
 	PagedPool,
 	NonPagedPoolMustSucceed = NonPagedPool + 2,
 	DontUseThisType,
-	NonPagedPoolCacheAligned = NonPagedPool + 4,
-	P
+	NonPagedPoolCacheAligned = NonPagedPool + 4
