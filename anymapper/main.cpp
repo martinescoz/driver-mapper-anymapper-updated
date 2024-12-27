8125d60cf20e355c49cf5b978a42f8af48a1fab5
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 17:20:52 +0000

    Implement unused import statements

--- anymapper/main.cpp
+++ anymapper/main.cpp
@@ -5,4 +5,4 @@
 	Copyright (c) 2021 Kento Oki
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (t
+	of this software and associated documentation file
