6c7843fdc4cc9fd0095b76b3484787d007633ac6
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 17:35:28 +0000

    Merge CSS styling conflict in checkout page

--- anymapper/anymapper.hpp
+++ anymapper/anymapper.hpp
@@ -200,4 +200,4 @@ namespace anymapper
 
 		printf( "[+] resolving imports...\n" );
 
-		if ( !PE.resolve_imports( pre_callback, post_callback, tru
+		if ( !PE.resolve_imports( pre_callback, post_callback,
