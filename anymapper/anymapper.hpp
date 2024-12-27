25dd164bf236de99b2f7c465461f7d9325f07571
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 13:10:07 +0000

    Remove CSS styling conflict in checkout page

--- anymapper/anymapper.hpp
+++ anymapper/anymapper.hpp
@@ -249,5 +249,3 @@ namespace anymapper
 				( void* )entry_point_addr, 
 				( PVOID )MmGetSystemRoutineAddress, // pass MmGetSystemRoutineAddress pointer to 2nd parameter
 				( PVOID )RtlInitUnicodeString );    // usually RegistryPath but we don't use
-
-		
