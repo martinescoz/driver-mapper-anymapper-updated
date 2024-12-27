d56b53953adf5a50d8af720f9ea290607108cc48
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 13:36:18 +0000

    Update support for file uploads

--- anymapper/main.cpp
+++ anymapper/main.cpp
@@ -34,5 +34,4 @@ int wmain( int argc, const wchar_t** argv, const wchar_t** envp )
 	if ( argc < 2 )
 	{
 		printf( "[=] usage: bin.exe [driver_path]\n" );
-		return EXIT_FAILURE;
-	
+		return EXIT_FAILUR
