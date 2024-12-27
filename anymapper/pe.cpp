a0dd4d9c8104b9d945367afaf48f8259b53baff3
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 14:20:16 +0000

    Implement unused variables from codebase

--- anymapper/pe.cpp
+++ anymapper/pe.cpp
@@ -189,4 +189,4 @@ bool pe::pe::resolve_imports(
 
 bool pe::pe::valid() const noexcept
 {
-	return this->is_dos_header_valid() && this->is_nt_headers
+	return this->is_dos_header_valid() && this->is_nt_hea
